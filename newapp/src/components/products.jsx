09dae9e418370ecf0fb9b09a44759d58a726c81b
Dedan Msafari <dedan.msafari@gmail.com>
2020-02-07 12:43:03 +0300

    completed products component

--- newapp/src/components/products.jsx
+++ newapp/src/components/products.jsx
@@ -1,25 +1,41 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 class Products extends Component {
+  render() {
+    const { title, price, imgUrl } = this.props.product;
+    console.log(this.props);
 
-    render() { 
-        const {title,price,imgUrl} = this.props.product
-       console.log(this.props);
-       
-        return ( 
-            <React.Fragment>
-              
-    
-<div className="card-fluid" style={{width: 200}}>
-<p className="card-title">{title}</p>
-<img className="card-img-top" src={imgUrl} alt="Product"/>
-<div className="card-body">
-<p className="card-text">Price:${price}</p>
-<a href="google.com"  className="btn btn-primary">Add to Wishlist</a>
-</div>
-</div>
-</React.Fragment>
-         );
-    }
+    return (
+      <React.Fragment>
+        <div className="card-fluid" style={{ width: 200 }}>
+          <p className="card-title">{title}</p>
+          <img className="card-img-top" src={imgUrl} alt="Product" />
+          <div className="card-body">
+            <p className="card-text">Price:${price}</p>
+            <div className="row">
+              <div className="col-sm-12 col-lg-6">
+              <button
+              className="btn btn-primary"
+              onClick={() => this.props.onWishlist(this.props.product)}
+            >
+              Add 
+            </button> 
+              </div>
+              <div className="col-sm-12 col-lg-6">
+              <button
+              className="btn btn-danger"
+              onClick={() => this.props.onRemove(this.props.product)}
+            >
+              Remove{" "}
+            </button>
+              </div>
+            </div>
+           
+           
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
 }
- 
+
 export default Products;
